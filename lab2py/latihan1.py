354cba2be46b247271de92e273fd0c8bb38c67d0
fakhriafif14 <147376559+fakhriafif14@users.noreply.github.com>
2023-10-20 08:37:41 +0700

    Add files via upload

---
+++ lab2py/latihan1.py
@@ -0,0 +1,15 @@
+# Mengambil input
+nama = input("Siapa nama kamu: ")
+umur = input("Berapa umur kamu: ")
+
+# Menampilkan output
+print("Hello",nama,"umur kamu adalah",umur,"tahun")
+
+print("hello")
+print("saya sedang belajar python")
+
+a = 8
+b = 6
+print ("variable a=",a)
+print ("variable b=",b)
+print ("hasil prnjumlahan a+b=",a+b)
